95c68736bf18140240d12ad7f3ba802d20f927fd
May <MayyFaucher@gmail.com>
2021-10-24 22:40:42 -0700

    added all the inquirer view routes: departments,employee,and roles

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require("inquirer");
-
+const util = require("util");
 
 const db = mysql.createConnection(
     {
@@ -11,16 +11,19 @@ const db = mysql.createConnection(
     },
     console.log("Connection to employee management system database established.\n\n"),
     console.log(
-    `     **************************************\n
+        `    ────────────────────────────────────────\n
      * * * Employee Management System * * *\n
-     ______________________________________\n
+    ────────────────────────────────────────\n
     `)
 );
+const dbquery = util.promisify(db.query).bind(db);
+
+
 
 const navMenu = ["View All Employees",
-                    "Add Employee", "Update Employee Role",
-                    "View All Roles", "Add Role",
-                    "View All Departments","Add Department","Quit"];
+    "Add Employee", "Update Employee Role",
+    "View All Roles", "Add Role",
+    "View All Departments", "Add Department", "Quit"];
 const questionsArr = [
     {
         type: "list",
@@ -30,21 +33,96 @@ const questionsArr = [
     }
 ];
 
-function bootSequence() {
-inquirer
-    .prompt(questionsArr)
-    .then(data => {
-        console.log(data)
 
-    })
-    .catch(err => console.log(err))
+function bootSequence() {
+    inquirer
+        .prompt(questionsArr)
+        .then(data => {
+            // console.log(data)
+            isolator(data)
+        })
+        .catch(err => console.log(err))
 }
 
 
+function isolator(data) {
+    // console.log("data be like:", data)
+    // I'm only using a switch because I need more experience with them,
+    // And it looks far nicer than like 300 if else statements lol.
+    switch (data.navAnswer) {
+        case 'View All Employees':
+            console.log("view them employees");
+            observeEmployees()
+            break;
+        case 'Add Employee':
+            console.log("add that employee");
+            fabricateEmployee()
+            break;
+        case 'Update Employee Role':
+            console.log("update that role");
+            modifyExistingHuman()
+            break;
+        case 'View All Roles':
+            console.log("view them roles");
+            percieveRoles()
+            break;
+        case 'Add Role':
+            console.log("add that role");
+            roleIntensification()
+            break;
+        case 'View All Departments':
+            console.log("view them departments");
+            detectDepartments()
+            break;
+        case 'Add Department':
+            console.log("add that department");
+            departmentAggrandizement()
+            break;
+        case 'Quit':
+            console.log("Let ME OUT of HERE");
+            console.log("NOTE TO SELF, find out how to exit aplication.");
+            break;
+        default:
+            console.log("Oh, wow, okay, that's a critical error. Don't know how you got this.")
 
+    }
+};
 
+function observeEmployees() {
+    db.query(`select * from employee`, (err, result) => {
 
+        if (err) { console.log(err, "Oh, retrieving Employees list errored.") };
 
+        console.table(result);
+
+        bootSequence();
+    })
+};
+
+function percieveRoles() {
+    db.query(`select * from roles`, (err, result) => {
+
+        if (err) { console.log(err, "Oh, retrieving Employees list errored.") };
+
+        console.table(result);
+
+        bootSequence();
+    })
+};
+
+function detectDepartments() {
+    db.query(`select * from departments`, (err, result) => {
+
+        if (err) { console.log(err, "Oh, retrieving Employees list errored.") };
+
+        console.table(result);
+
+        bootSequence();
+    })
+};
+// async function detectDepartments() {
+//     db.query
+// }
 
 
 
---
+++ schema.sql
@@ -0,0 +1,30 @@
+-- Employee Management System DB
+drop database if exists ems_db;
+create database ems_db;
+use ems_db;
+
+create table departments (
+    id int not null primary key,
+    department_name varchar(30)
+
+);
+
+create table roles (
+    id int not null primary key,
+    title varchar(30) not null,
+    salary decimal not null,
+    department_id int,
+    foreign key (department_id)
+    references departments(id)
+);
+-- MAYBE add an ismanager boolean
+create table employee (
+    id int primary key,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int,
+    manager_id int,
+    foreign key (role_id)
+    references roles(id)
+);
+
